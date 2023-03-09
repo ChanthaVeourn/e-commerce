8303d4f11313bf037f4f5bfd63d50c57af5a35b7
Lyheng <senglyheng2001@gmail.com>
2023-03-09 11:26:15 +0700

    update resposive

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,10 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading,Text } from "@chakra-ui/react";
 import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <>
-      <Box  boxShadow="dark-lg">
+      <Box boxShadow="dark-lg">
         <footer className="relative pt-8 pb-6">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap text-left lg:text-left">
@@ -13,8 +13,7 @@ export default function Footer() {
                   Cool Ecommerce
                 </Heading>
                 <h5 className="text-lg mt-0 mb-2 ">
-                  Find us on any of these platforms, we respond 1-2 business
-                  days.
+                  Welcome to my Website.....
                 </h5>
                 {/* icon */}
                 <div className="mt-6 lg:mb-0 mb-6 flex">
@@ -46,7 +45,16 @@ export default function Footer() {
               </div>
               <div className="w-full lg:w-6/12 px-4">
                 <div className="flex flex-wrap items-top mb-6">
-                  <div className="w-full lg:w-4/12 px-4 ml-auto">
+                  <div className="w-full text-center px-4 ml-auto">
+                    <Text className="block  text-blueGray-500 text-2xl font-semibold mb-2 text-orange-300" >
+                      Address
+                    </Text>
+                    <p>                      
+                      Royal University of Phnom Penh (RUPP) Russian Federation
+                      Boulevard, Toul Kork, Phnom Penh, Cambodia.{" "}
+                    </p>
+                  </div>
+                  {/* <div className="w-full lg:w-4/12 px-4">
                     <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-orange-400">
                       Useful Links
                     </span>
@@ -76,40 +84,13 @@ export default function Footer() {
                         Free Product
                       </Link>
                     </ul>
-                  </div>
-                  <div className="w-full lg:w-4/12 px-4">
-                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-orange-400">
-                      Useful Links
-                    </span>
-                    <ul>
-                      <Link
-                        to={"/"}
-                        className=" hover:text-gray-600 font-semibold block pb-2 text-sm"
-                      >
-                        About Us
-                      </Link>
-                      <Link
-                        to={"/"}
-                        className="hover:text-gray-600 font-semibold block pb-2 text-sm"
-                      >
-                        Blog
-                      </Link>
-                      <Link
-                        to={"/"}
-                        className="hover:text-gray-600 font-semibold block pb-2 text-sm"
-                      >
-                        Github
-                      </Link>
-                      <Link
-                        to={"/"}
-                        className="hover:text-gray-600 font-semibold block pb-2 text-sm"
-                      >
-                        Free Product
-                      </Link>
-                    </ul>
-                  </div>
+                  </div> */}
                 </div>
               </div>
+            </div>
+            <hr className="my-5" />
+            <div className="text-center">
+              <p>Copyright © 2023 Cool-Ecommerce - By E8 - Group 7.</p>
             </div>
           </div>
         </footer>
